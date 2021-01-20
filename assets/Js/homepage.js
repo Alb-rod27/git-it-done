663cbc83e5fa4dab7229f14426826afd2f18bc72
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-01-20 13:59:33 -0800

    checking code

---
+++ assets/Js/homepage.js
@@ -0,0 +1,9 @@
+let getUserRepos = function() {
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        console.log("inside", response);
+      });
+      
+    }
+  
+    console.log("outside");
+  getUserRepos();
